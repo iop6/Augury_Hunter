eadcb1621119f8e909f995d2650b3a60e5468877
Michael Zalud <mazalud@gmail.com>
2021-02-05 00:22:39 +0000

    Update to Augury_Puller script. Added specific file location functionality.

--- Augury_Puller.py
+++ Augury_Puller.py
@@ -1,23 +1,25 @@
 import requests
+import os
 
+API_key = input("What is you API key? ")
 running = True
 while running == True:
   def Puller():
     query_id_list = []
-    open("mass_pull.json", "w")
-    with open("Query_ID_File", "r") as f:
+    with open("Query_ID_File.txt", "r") as f:
       for line in f:
         query_id_list.append(line)
         var = str(line)
         url = "https://augury5.heliumrain.com/api/results/" + var.rstrip('\n') + "?format=json"
         print (url)
         payload = ""
-        headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": "8397cb23f42e8a8ca73d8b7a5098ce02afbce154"}
+        headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": API_key}
         response = requests.request("GET", url, data=payload, headers=headers)
+        filename = line.rstrip('\n') + ".json"
+        file = open(os.path.join('C:\Data\Augury\Flows',filename), "w")
 
-        file = open("mass_pull.json", "a")
         file.write(response.text)
-      file.close()  
+      file.close()
 
   Puller()
   running = False
