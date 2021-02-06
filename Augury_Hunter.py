2d056820062d86f14a33f4ccb0f6f1c4161c3437
Isaac Roberts <isaacrobertsemail@gmail.com>
2021-02-06 01:11:01 +0000

    Efficiency Change

--- Augury_Hunter.py
+++ Augury_Hunter.py
@@ -1,24 +1,26 @@
 import requests
 import json
 #-------------------------------------------------------------------------------------------------------------------------------------
+#Test query 11645485
 url = "https://augury5.heliumrain.com/api"
 payload = ""
-auth = str(input("What is your auth key? "))
-headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": auth}
+#auth = str(input("What is your auth key? "))
+headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": "8397cb23f42e8a8ca73d8b7a5098ce02afbce154"}
 running = True
 #-------------------------------------------------------------------------------------------------------------------------------------
 while running == True:
 
   def Get_Bi_Directional():
-    query_id = str(input("What is your query ID? "))
+    #query_id = str(input("What is your query ID? "))
     src_dst = []
     flipped_ips = []
     detailed_lines = []
     BD_list = []
+    Var_list = []
 
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-    results = requests.request("GET", url+"/results/" + query_id + "?format=json", data=payload, headers=headers)
+    results = requests.request("GET", url+"/results/" + "11645485" + "?format=json", data=payload, headers=headers)
     BD_file = open("Bi_Directional.json", "w")
     BD_file.write(results.text)
     BD_file.close()
@@ -47,16 +49,37 @@ while running == True:
         src_dst.append(line)
         flipper = dst_ip + "   " + src_ip
         flipped_ips.append(flipper)
-        line_details = timestamp + "," + src_ip + "," + src_cc + "," + proto + ","  + dst_ip + ","  + dst_cc + "," + proto + "," + dst_port + "," + src_port + "," + tcp_flags + "," + num_pkts + "," + num_octets
+        line_details = timestamp + "," + src_ip + "," + src_cc + "," + proto + ","  + dst_ip + ","  + dst_cc + "," + proto + "," + dst_port + "," + src_port + "," + tcp_flags + "," + num_pkts + "," + num_octets + "\n"
         detailed_lines.append(line_details)
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-
+    '''
     x = 0
     for line in src_dst:
-        for line in src_dst:
-            if line == flipped_ips[x]:
+        y = x + 1
+        while y < len(flipped_ips):
+            if line == flipped_ips[y]:
                 BD_list.append(detailed_lines[x])
+            y += 1
+        x += 1
+
+    with open("Bi_Directional.csv", "w") as f:
+        for line in BD_list:
+            f.write(line)
+    f.close()
+    '''
+    x = 0
+    for line in src_dst:
+        y = x + 1
+        while y < len(flipped_ips):
+            if line in Var_list:
+                print("Howdy")
+                break
+            if line == flipped_ips[y]:
+                print("Test")
+                Var_list.append(src_dst[x])
+                BD_list.append(detailed_lines[x])
+            y += 1
         x += 1
 
     with open("Bi_Directional.csv", "w") as f:
